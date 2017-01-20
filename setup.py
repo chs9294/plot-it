95e7a277c634daa1992800dc89c368f3a6a53996
chs9294 <chs9294@yahoo.com>
2017-01-20 11:50:10 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PLOTIT',
+      version='0.1.alpha',
+      description='The plotting package',
+      author='Abhishek Chougule',
+      author_email='chs9294@yahoo.com',
+      url='https://github.com/chs9294/plot-it.git',
+      scripts=['PLOTIT']
+     )
