346f3aab865ff222ebe05c62d8e2d49099503400
chs9294 <chs9294@yahoo.com>
2017-01-20 12:15:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='PLOTIT',
+setup(name='Plotit',
       version='0.1.alpha',
       description='The plotting package',
       author='Abhishek Chougule',
       author_email='chs9294@yahoo.com',
       url='https://github.com/chs9294/plot-it.git',
-      scripts=['PLOTIT']
+      scripts=['Plotit']
      )
